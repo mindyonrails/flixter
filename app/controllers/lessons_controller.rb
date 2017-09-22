35a98f96fa38537cc635e18bc62c606c2762beae
Mindy Chen <mindyonrails@gmail.com>
2017-09-22 03:14:19 +0000

    challenge permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,19 @@
 class LessonsController < ApplicationController
+before_action :authenticate_user!
+before_action :require_authorized_for_current_lesson, only: [:show]
+
+
   def show
   end
 
 
   private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      redirect_to courses_path, alert: 'You must be logged in to view this course'    
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
